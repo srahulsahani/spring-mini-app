31f43439ed0a4b32aa32fee38fa95aa0e78e22d7
Rahul Sahani <srahulsahani@gmail.com>
2023-12-19 23:45:39 +0530

    get current loggedIn User details

--- springsecurity/src/main/java/com/jwt/springsecurity/controller/HomeController.java
+++ springsecurity/src/main/java/com/jwt/springsecurity/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -20,5 +21,10 @@ public class HomeController {
     @GetMapping("/users")
     public List<User> getUser(){
         return userService.getUsers();
+    }
+
+    @GetMapping("/current-user")
+    public String getLoggedInUser(Principal principal){
+        return principal.getName();
     }
 }
