947f0266050fc85c86f727c54e58cacb1b464c6c
Rahul Sahani <srahulsahani@gmail.com>
2023-12-25 16:56:15 +0530

    Jwt Implementation

---
+++ springsecurity/src/main/java/com/jwt/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jwt.springsecurity.config;
+
+
+import com.jwt.springsecurity.security.JwtAuthenticationEntryPoint;
+import com.jwt.springsecurity.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf-> csrf.disable())
+                .cors(cors-> cors.disable())
+                .authorizeHttpRequests(auth->
+                        auth.requestMatchers("/home/**").authenticated()
+                                .requestMatchers("/auth/login").permitAll()
+                                .anyRequest().authenticated())
+                .exceptionHandling(ex->ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
